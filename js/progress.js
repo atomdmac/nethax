cf92dd13cf540221ac6b852008129b89ae12a35c
suicidepills <atom.mac@gmail.com>
2014-02-16 11:35:15 -0500

    Convert progress.js to use Canvas, center title.
    Signed-off-by: suicidepills <atom.mac@gmail.com>

--- js/progress.js
+++ js/progress.js
@@ -1,7 +1,7 @@
 Crafty.c("ProgressBar", {
     
     _background: null,
-    _forground: null,
+    _foreground: null,
     _title: null,
     
     _min: 0,
@@ -25,6 +25,8 @@ Crafty.c("ProgressBar", {
     
     setTitle: function (value) {
         this._title.text(value);
+        this._title.x = (this._background._w - this._title._w) / 2;
+        this._title.y = (this._background._h - this._title._h) / 2;
         return this;
     },
     
@@ -39,22 +41,41 @@ Crafty.c("ProgressBar", {
     },
     
     init: function () {
-        this.requires("2D, DOM");
+        this.requires("2D, Canvas");
         
-        this._background = Crafty.e("2D, DOM, Color")
-            .css("border", "1px solid gray");
-        this._foreground = Crafty.e("2D, DOM, Color")
+        // Replace draw() with our own implementation.
+        this._draw = this.draw;
+        this.draw  = function (ctx, x, y, w, h) {
+            ctx = ctx || Crafty.canvas.context;
+            this._draw(ctx);
+        };
+        
+        this._background = Crafty.e("2D, Canvas, Color")
+            .color("#666");
+        
+        this._foreground = Crafty.e("2D, Canvas, Color")
             .color("#ff0000");
-        this._title = Crafty.e("DOM, Text")
-            .text("Progress")
-            .textColor("#fff");
-            
+        
+        this._title = Crafty.e("2D, Canvas, Text")
+            .textColor("#ffffff", 1);
+        this._title.text("Progress");
+        
+        this._background.z = 0;
+        this._foreground.z = 1;
+        this._title.z      = 2;
+        
         this.attach(
             this._background,
             this._foreground,
             this._title
-        )
+        );
         
         this.bind("Change", this._onChange);
+        
+        // Set default dimensions.
+        this.attr({
+            w: 100,
+            h: 20
+        });
     }
 });
